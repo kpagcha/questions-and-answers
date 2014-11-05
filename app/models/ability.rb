e00b3ccdbbacdb7cec674bb3b1c58a0ceb72b620
pablo <4d.garcia6@gmail.com>
2014-11-05 20:16:45 +0100

    main page started

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,13 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    user ||= User.new
+    if user.admin?
+        can :manage, :all
+    else
+        can :read, :all
+    end
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_and_belongs_to_many :roles, :join_table => :users_roles
+
+  def admin?
+    admin_role = Role.where("name", "=", "admin").pluck(:id)
+    return self.roles.include?(admin_role)
+  end
 end
