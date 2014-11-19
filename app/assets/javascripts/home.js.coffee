afe1989d430f9b6c8376bbe07cca0e26c2e77df7
pablo <4d.garcia6@gmail.com>
2014-11-19 22:26:27 +0100

    script finally fixed

--- app/assets/javascripts/home.js.coffee
+++ app/assets/javascripts/home.js.coffee
@@ -1,11 +1,8 @@
 $(document).on "submit", "form[name=upvote-form]", ->
   form = $(this)
-  $.post "/vote", $(this).serialize(), (data) ->
-    if data["status"] is true
-      form.find("input").addClass "disabled"
-      $("#vote-count-".concat(form.find("#question_id").attr("value"))).html data["votes"]
-    else
-
+  +$.post("/vote", $(this).serialize(), (data) ->
+    form.find("input").addClass "disabled"
+    $("#vote-count-".concat(form.find("#question_id").attr("value"))).html data["votes"]
     return
-
+  )
   return
--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -4,18 +4,16 @@ class VoteController < ApplicationController
 		question_id = params[:question][:id]
 		user_id = current_user.id
 		vote = Vote.where(["question_id = :q", { q: question_id }]).where(["user_id = :u", { u: user_id }]).take
+		50.times{p vote}
 		respond_to do |format|
 			if vote.nil?
 				@vote = Vote.new
 				@vote.question = Question.find question_id
 				@vote.user = User.find user_id
 				@vote.save
-				votes = Vote.where(["question_id = :q", { q: question_id }]).count
-				format.json { render :json => { status: true, votes: votes } }
-			else
-				format.json { render :json => { status: false } }
-				#format.json { head :ok }
 			end
+			votes = Vote.where(["question_id = :q", { q: question_id }]).count
+			format.json { render :json => { status: true, votes: votes } }
 		end
 	end
 end
